2240a30cffc2cf492ce1e82dd10d8ed6a1a8daa6
yujingbo <1935952410@qq.com>
2019-08-02 10:38:09 +0800

    x

---
+++ 04-vue/20-Tabbar,Header,Cell/src/router.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+import Index from './views/Index.vue'
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "index",
+      component: Index,
+      children: [
+        {
+          path: "",
+          redirect:"/wchart"
+        },
+        {
+          path: "/wchart",
+          name: "wchart",
+          component: () => import("./views/Wchart.vue")
+        },
+        {
+          path: "/abook",
+          name: "abook",
+          component: () => import("./views/AddressBook.vue")
+        },
+        {
+          path: "/find",
+          name: "find",
+          component: () => import("./views/Find.vue")
+        },
+        {
+          path: "/mine",
+          name: "mine",
+          component: () => import("./views/Mine.vue")
+        }
+      ]
+    }, {
+      path: "/register",
+      name: "register",
+      component: () => import("./views/Register.vue")
+    }, {
+      path: "/login",
+      name: "login",
+      component: () => import("./views/Login.vue")
+    }
+  ]
+})
---
+++ 04-vue/20-Tabbar,Header,Cell/src/views/Mine.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <Header title="我的"></Header>
+    </div>
+</template>
+
+<script>
+import Header from "../components/Header"
+export default{
+    components:{
+        Header
+    }
+}
+</script>
