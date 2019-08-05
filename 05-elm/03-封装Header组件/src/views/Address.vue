6808925294bad0d0fcea2ac5d2203afefbda9933
yujingbo <1935952410@qq.com>
2019-08-05 11:18:39 +0800

    x

---
+++ 05-elm/03-封装Header组件/src/views/Address.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    <Header :isLeft="true" title="选择收货地址"></Header>
+</div>
+</template>
+
+<script>
+import Header from "../components/Header"
+
+export default{
+    components:{
+        Header
+    }
+}
+</script>
